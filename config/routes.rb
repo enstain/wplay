5b8faa78b8784e4ae5276bdb51c8f65218b2fe32
enstain <company_nbm@mail.ru>
2014-06-29 16:48:20 +0400

    nokogiri

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Wplay::Application.routes.draw do
  
-  get "control" to: "control#index" as: "control_index"
-  get "control/invite"
   devise_for :workers
   devise_for :admins
   mount RailsAdmin::Engine => '/adminka', as: 'rails_admin'
+
+  get "control", to: "control#index", as: "control_index"
+  get "control/invite"
 
   root to: "board#index"
 end
