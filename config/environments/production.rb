cbd8b4f58ab4fe9c656d088fc7f21c208a066294
enstain <company_nbm@mail.ru>
2014-07-02 16:44:54 +0400

    sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,9 @@ Wplay::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  #config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 25 }
+  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.smtp_settings = { :address => "localhost", :port => 25 }
 
   # Enable threaded mode
   # config.threadsafe!
